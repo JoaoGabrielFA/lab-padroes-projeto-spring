3b4c677712b3049a373583f1b3158248ac263b12
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2024-11-17 10:20:00 -0300

    Removendo informações redundantes.

--- src/main/java/one/digitalinnovation/gof/model/Endereco.java
+++ src/main/java/one/digitalinnovation/gof/model/Endereco.java
@@ -17,15 +17,10 @@ public class Endereco {
 
 	@Id
 	private String cep;
-	private String logradouro;
-	private String complemento;
 	private String bairro;
 	private String localidade;
 	private String uf;
-	private String ibge;
-	private String gia;
 	private String ddd;
-	private String siafi;
 
 	public String getCep() {
 		return cep;
@@ -33,22 +28,6 @@ public class Endereco {
 
 	public void setCep(String cep) {
 		this.cep = cep;
-	}
-
-	public String getLogradouro() {
-		return logradouro;
-	}
-
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
-
-	public String getComplemento() {
-		return complemento;
-	}
-
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
 	}
 
 	public String getBairro() {
@@ -75,22 +54,6 @@ public class Endereco {
 		this.uf = uf;
 	}
 
-	public String getIbge() {
-		return ibge;
-	}
-
-	public void setIbge(String ibge) {
-		this.ibge = ibge;
-	}
-
-	public String getGia() {
-		return gia;
-	}
-
-	public void setGia(String gia) {
-		this.gia = gia;
-	}
-
 	public String getDdd() {
 		return ddd;
 	}
@@ -98,13 +61,4 @@ public class Endereco {
 	public void setDdd(String ddd) {
 		this.ddd = ddd;
 	}
-
-	public String getSiafi() {
-		return siafi;
-	}
-
-	public void setSiafi(String siafi) {
-		this.siafi = siafi;
-	}
-
 }
